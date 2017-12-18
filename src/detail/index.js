b5a6054995218b4cd375ce498a23643a37bd725c
Nick Kinlen <nick_kinlen@hotmail.com>
2017-12-18 05:27:34 -0500

    Added Search Result h2 and tweeked details in info section

--- src/detail/index.js
+++ src/detail/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from '../header';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
+import Footer from '../Footer.js';
+import Searchbar from '../header/searchbar.js';
 
 /* My Movie API key : https://api.themoviedb.org/3/movie/550?api_key=03b9a40695aae1f4e99a42e90e012e9e
   Movies in theaters now /discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22
@@ -9,6 +11,7 @@ import axios from 'axios';
   https://api.themoviedb.org/3/movie/now_playing?api_key=03b9a40695aae1f4e99a42e90e012e9e&language=en-US&page=1
 */
 
+// Searchbar.js uses this.state.query to store the search input
 
 class Detail extends Component {
 	constructor(props) {
@@ -22,12 +25,10 @@ class Detail extends Component {
 	componentDidMount() {
 	  axios.get(`https://api.themoviedb.org/3/search/multi?api_key=03b9a40695aae1f4e99a42e90e012e9e&language=en-US&query=justice%26league&page=1&include_adult=false`)
 	    .then(res => {
-	      const data = res.data.results.map(obj => ({title: obj.title, overview: obj.overview, poster: obj.poster_path, vote: obj.vote_average, release: obj.release_date}));
+	      const data = res.data.results.map(obj => ({title: obj.title, overview: obj.overview, poster: obj.poster_path, vote: obj.vote_average, release: obj.release_date, type: obj.media_type}));
 	      this.setState({ data });
 	    });
 	}
-
-
 
 	render() {
 		return(
@@ -36,6 +37,8 @@ class Detail extends Component {
 
 				<div className="details-container">
 
+				<h2>Search Results: </h2>
+
 				<ul>
 					{this.state.data.map(function(post, index){
 						return (
@@ -43,20 +46,22 @@ class Detail extends Component {
 
 									<h3>{post.title}</h3>
 
-									<img src={"https://image.tmdb.org/t/p/w185/" + post.poster} alt="Movie Poster" />
+									<img src={"https://image.tmdb.org/t/p/w300/" + post.poster} alt="Movie Poster" />
 
 									<div className="info">
 										<p>6.8</p>
 										<p>PG-13</p>
-										<p>120 min</p>
+										<p>{post.type}</p>
+										<p>{post.language}</p>
+										<p>{post.country}</p>
 
-										<p>Directed by</p>
+										<p class="italic">Directed by:</p>
 										<p>Zack Snyder</p>
 
-										<p>Written by</p>
+										<p class="italic">Written by:</p>
 										<p>Chris Terrio</p>
 
-										<p>Starring</p>
+										<p class="italic">Starring:</p>
 										<ul>
 											<li>Ben Affleck</li>
 											<li>Henry Cavil</li>
